c175730fa7e51aad8031b8dbdacc1885ec092304
jumaniyozov <jumaniyozovse@gmail.com>
2024-12-10 16:54:27 +0500

    trying gcp paas

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jumaniyozov/greenfield/handlers"
 	"github.com/jumaniyozov/greenfield/handlers/rest"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	addr := ":8080"
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if addr == ":" {
+		addr = ":8080"
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck)
 	log.Printf("listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
